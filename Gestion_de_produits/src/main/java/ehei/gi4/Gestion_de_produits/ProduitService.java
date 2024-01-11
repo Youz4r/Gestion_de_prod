10a3049341802652ccc7c07e1639067496d3c8d4
Youz4r <youssef.belbakkal.48@gmail.com>
2024-01-11 18:54:49 +0100

    modification

--- Gestion_de_produits/src/main/java/ehei/gi4/Gestion_de_produits/ProduitService.java
+++ Gestion_de_produits/src/main/java/ehei/gi4/Gestion_de_produits/ProduitService.java
@@ -1,9 +1,9 @@
 package ehei.gi4.Gestion_de_produits;
+import java.util.*;
 
 public class ProduitService {
 	private Map<Long, Produit> produits = new HashMap<>();
 	 public void ajouterProduit(Produit produit) throws Exception {
-	        verifierUniciteEtValidite(produit);
 	        produits.put(produit.getId(), produit);
 	    }
 
