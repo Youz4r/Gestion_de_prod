1dabef9fbee8e05277f1ce9719200f476957f6f8
Youz4r <youssef.belbakkal.48@gmail.com>
2024-01-12 22:54:15 +0100

    fixation de bug

--- src/main/java/ehei/gi4/Gestion_de_produits/Produit.java
+++ src/main/java/ehei/gi4/Gestion_de_produits/Produit.java
@@ -1,47 +1,48 @@
 package ehei.gi4.Gestion_de_produits;
 
 public class Produit {
-    private Long id;
-    private String nom;
-    private double prix;
-    private int quantite;
+	    private long id;
+	    private String nom;
+	    private double prix;
+	    private int quantite;
 
-    public Produit(Long id, String nom, double prix, int quantite) {
-        this.id = id;
-        this.nom = nom;
-        this.prix = prix;
-        this.quantite = quantite;
-    }
+	    public Produit(Long id, String nom, double prix, int quantite) {
+	        this.id = id;
+	        this.nom = nom;
+	        this.prix = prix;
+	        this.quantite = quantite;
+	    }
 
-    public Long getId() {
-        return id;
-    }
+	    public Long getId() {
+	        return id;
+	    }
 
-    public String getNom() {
-        return nom;
-    }
+	    public String getNom() {
+	        return nom;
+	    }
 
-    public double getPrix() {
-        return prix;
-    }
+	    public double getPrix() {
+	        return prix;
+	    }
 
-    public int getQuantite() {
-        return quantite;
-    }
+	    public int getQuantite() {
+	        return quantite;
+	    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
+	    public void setNom(String nom) {
+	        this.nom = nom;
+	    }
 
-    public void setPrix(double prix) {
-        this.prix = prix;
-    }
+	    public void setPrix(double prix) {
+	        this.prix = prix;
+	    }
 
-    public void setQuantite(int quantite) {
-        this.quantite = quantite;
-    }
+	    public void setQuantite(int quantite) {
+	        this.quantite = quantite;
+	    }
+
 }
